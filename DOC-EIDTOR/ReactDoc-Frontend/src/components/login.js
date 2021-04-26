bd654a1208dbe6a6763cf21d74c2ef70e545b310
Prathibha-11014075 <11014075@stud.hochschule-heidelberg.de>
2021-04-26 16:54:27 +0200

    Integration of registration UI to login screen and Implementation of Oauth authentication

--- DOC-EIDTOR/ReactDoc-Frontend/src/components/login.js
+++ DOC-EIDTOR/ReactDoc-Frontend/src/components/login.js
@@ -1,10 +1,58 @@
 import { Button } from '@material-ui/core';
 import React from 'react';
+
+import { auth, provider } from '../firebase';
+import { useStateValue } from '../StateProvider';
+import { actionTypes } from '../Reducer';
 import './login.css';
 import logo from './images/logo.png';
 
+
 const Login = () => {
+
+  const [state, dispatch] = useStateValue();
+  const [showResults, setShowResults] = React.useState(false);
+  const [buttonText, setButtonText] = React.useState("Login"); 
+  const [ifRegBtnText, setIfRegBtnText] = React.useState("Not Registered?");  
+  const [registerBtnTxt, setRegisterBtnTxt] = React.useState("Create an Account");
+  const handleClick = () => {
+    switch (registerBtnTxt) {
+      case "Create an Account":
+        setShowResults(true);
+        setButtonText("Create");
+        setRegisterBtnTxt("Login");
+        setIfRegBtnText("Registered?");
+        break;
+      case "Login":
+        setShowResults(false);
+        setButtonText("Login");
+        setRegisterBtnTxt("Create an Account");
+        setIfRegBtnText(" Not Registered?");
+        break;
+      default:
+        setButtonText("Create an Account");
+        break;
+    }
+    
+
+  };
   
+
+  const signIn = () => {
+    auth
+      .signInWithPopup(provider)
+      .then((result) => {
+        console.log(result);
+
+        dispatch({
+          type: actionTypes.SET_USER,
+          user: result.user,
+        });
+      })
+      .catch((error) => alert(error.message));
+  };
+
+ 
   return (
     <div className='login'>
       <div className='login_logo'>
@@ -19,15 +67,17 @@ const Login = () => {
 
       </div>
       <div className="login-show">
-        <h3>Login</h3>
-        <input type="text" placeholder="Email" />
+        <input type="text" placeholder="Username" />
         <input type="password" placeholder="Password" />
-        <input type="button" value="sign-In" />
+        { showResults ? <input type="text" placeholder="Email ID" /> : null }
+        <button type="submit" >{buttonText}</button>
+        <p1>{ifRegBtnText}<a href="#" onClick={() => handleClick()}> {registerBtnTxt}</a></p1>
+        
 
       </div>
       <div className='login-via'>
         <p4>OR </p4>
-        <input type="button" value="Sign in with Google"  />
+        <input type="button" value="Sign in with Google" onClick={signIn} />
 
       </div>
 
